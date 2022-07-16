ed81914ef9eff275c15c0eed364797b557a42015
DefaultSpieleR <yashzedd@gmail.com>
2022-07-16 16:24:41 +0530

    Update twoSum.py
    using the dictionary

--- twoSum.py
+++ twoSum.py
@@ -2,6 +2,7 @@
 
 arr = [2,4,-3,7,8,5]
 target = 15
+
 # brute force - 2 loops - time complexity - O(n^2) space complexity - O(1)
 
 def BruteForce(arr, target):
@@ -11,4 +12,20 @@ def BruteForce(arr, target):
 			if arr[i] + arr[j] == target:
 				return [i, j]
 
-print(BruteForce(arr, target))
+#print(BruteForce(arr, target))
+
+def usingDic(arr, target):
+	hashDic = { }
+	for i in range(len(arr)):
+		hashDic[arr[i]] = i
+
+	for i, val in enumerate(hashDic):
+
+		toFind = target - val
+
+		if toFind in hashDic:
+			print(i)
+	
+
+usingDic(arr, target)
+
